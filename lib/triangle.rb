5528b3e62240fd4ccb9ad1fca0c8493b4c75e9af
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 13:45:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
         raise PartnerError
       rescue PartnerError => error
           puts error.message
-      endraise TriangleError
+      end
     elsif (length_1 + length_2 <= length_3) || (length_2 + length_3 <= length_1) || (length_3 + length_1 <= length_2)
       begin
         raise PartnerError
