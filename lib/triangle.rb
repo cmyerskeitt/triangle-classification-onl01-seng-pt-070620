bce0cd660b92ca9c079dfd2c175d41ea309d80a2
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 13:19:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,5 +8,11 @@ class Triangle
   end
   
   def kind 
+    length_1 + length_2 = length_3
+    length_2 + length_3 = length_1
+    length_3 + length_1 = length_2
     
+    length_1 > 0 
+    length_2 > 0 
+    length_3 > 0 
 end
