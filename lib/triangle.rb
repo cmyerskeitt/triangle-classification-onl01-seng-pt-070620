2bada380a706980ed147e4e23302696160f96fa9
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 14:28:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,13 +9,9 @@ class Triangle
   
   def kind 
     if (length_1 <= 0) || (length_2 <= 0) || (length_3 <= 0)
-      begin
         raise TriangleError
-      end
     elsif (length_1 + length_2 <= length_3) || (length_2 + length_3 <= length_1) || (length_3 + length_1 <= length_2)
-      begin
         raise TriangleError
-      end
     else
       if (@length_1 == @length_2) && (@length_2 == @length_3)
         :equilateral 
