ed5ced681d89b563e6ce546ba6d5f116bc7a0851
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 14:26:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,14 +11,10 @@ class Triangle
     if (length_1 <= 0) || (length_2 <= 0) || (length_3 <= 0)
       begin
         raise TriangleError
-       TriangleError => error
-          puts error.message
       end
     elsif (length_1 + length_2 <= length_3) || (length_2 + length_3 <= length_1) || (length_3 + length_1 <= length_2)
       begin
         raise TriangleError
-       TriangleError => error
-          puts error.message
       end
     else
       if (@length_1 == @length_2) && (@length_2 == @length_3)
