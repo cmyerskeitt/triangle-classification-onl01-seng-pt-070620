d318019cd4b2a7176db4a52bc6607c468512bf0e
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 13:14:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+  
+  def initialize(length_1, length_2, length_3)
+    @length_1= length_1
+    @length_2= length_2
+    @length_3= length_3
+  end
+  
+  def kind 
+    
 end
