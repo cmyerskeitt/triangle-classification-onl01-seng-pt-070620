595e56f02a97df67dcbace9afc227963848aa4d5
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 13:31:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,12 @@ class Triangle
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral 
       elseif 
-        
+        elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
    
     
      class TriangleError < StandardError
