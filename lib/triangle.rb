3692fa19e5dd23df6be7a8625af938ba345ba1ce
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 13:52:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,6 +20,12 @@ class Triangle
       rescue TriangleError => error
           puts error.message
       end
+    elsif (length_1 = 0) && (length_2 = 0) && (length_3 = 0)
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end 
     else
       if (@length_1 == @length_2) && (@length_2 == @length_3)
         :equilateral 
