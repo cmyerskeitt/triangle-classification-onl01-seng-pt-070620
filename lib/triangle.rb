af26b35bb9a36666f359d07b4127987dcaaee960
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 13:38:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
         :equilateral 
       elsif (@length_1 == @length_2) || (@length_2 == @length_3) || (@length_1 == @length_3)
         :isosceles
-      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+      elsif (@length_1 != @length_2) && (@length_2!= @length_3) && (@length_1 != @length_3)
         :scalene
       end
     end
