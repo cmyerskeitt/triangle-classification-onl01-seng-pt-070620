eb1d4847eeb392c80c41b9f45696222940df8f7c
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-09 13:47:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,7 @@ class Triangle
       rescue TriangleError => error
           puts error.message
       end
+      
     elsif (length_1 + length_2 <= length_3) || (length_2 + length_3 <= length_1) || (length_3 + length_1 <= length_2)
       begin
         raise TriangleError
